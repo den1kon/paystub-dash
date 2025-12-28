d61ea178832f8392fb457157b0c56b750eb6ae23
den1kon <dkondratiuk06@gmail.com>
2025-12-28 14:27:28 +0100

    fix(backend): cleanup main.ts and add seeding logic

--- backend/main.ts
+++ backend/main.ts
@@ -1,6 +1,4 @@
 import Db from "./db/database.ts";
-import { CompanyModel, CompanyEntity } from "./models/company-model.ts";
-import { toDTO } from "./controllers/company-controller.ts";
 import { createApp } from "./app.ts";
 
 
@@ -9,16 +7,7 @@ dbInstance.init();
 
 const conn = dbInstance.getConnection();
 
-const companyModel = new CompanyModel(conn);
-
-companyModel.create("Test Company");
-
-const companiesModel: CompanyEntity[] = companyModel.getAll();
-console.log("Raw Companies:", companiesModel);
-
-const companiesDTO = companiesModel.map(toDTO);
-console.log("Company DTOs:", companiesDTO);
-
+// Db.seedCompanies(conn);
 
 const app = createApp(conn);
 
