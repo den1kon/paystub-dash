03d20016579edc334060b75971476f40bce34d83
den1kon <dkondratiuk06@gmail.com>
2025-12-28 09:43:29 +0100

    feat(backend): separate companies entity to dto map logic into mappers/company.ts

---
+++ backend/mappers/company.ts
@@ -0,0 +1,8 @@
+import { CompanyDTO } from "../dtos/company-dto.ts";
+import { CompanyEntity } from "../models/company-model.ts";
+
+export const toDTO = (entity: CompanyEntity): CompanyDTO => ({
+  id: entity.id,
+  name: entity.name,
+  isDeleted: Boolean(entity.is_deleted),
+});
