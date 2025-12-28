4437a678299cc92e4ba71fc21890a49c41412db0
den1kon <dkondratiuk06@gmail.com>
2025-12-28 09:45:52 +0100

    feat(backend): apply logger midleware and add crud company routes to application

--- backend/app.ts
+++ backend/app.ts
@@ -1,8 +1,14 @@
 import { Application } from "jsr:@oak/oak/application";
 import { Router } from "jsr:@oak/oak/router";
 import { CompanyModel } from "./models/company-model.ts";
-import { makeGetAllCompaniesResponse } from "./controllers/company-controller.ts";
+import {
+  makeCreateCompanyResponse,
+  makeDeleteCompanyResponse,
+  makeGetAllCompaniesResponse,
+  makeUpdateCompanyNameResponse,
+} from "./controllers/company-controller.ts";
 import { Database } from "@db/sqlite";
+import { logger } from "./middleware/logger.ts";
 
 export function createApp(conn: Database): Application {
   const companyModel = new CompanyModel(conn);
@@ -14,8 +20,22 @@ export function createApp(conn: Database): Application {
   });
 
   router.get("/api/v0/companies", makeGetAllCompaniesResponse(companyModel));
+  router.post("/api/v0/companies", makeCreateCompanyResponse(companyModel));
+  router.put("/api/v0/companies", makeUpdateCompanyNameResponse(companyModel));
+  router.delete(
+    "/api/v0/companies/:id",
+    makeDeleteCompanyResponse(companyModel),
+  );
+
+  router.delete('users/:userId', (ctx) => {
+    const userId = ctx.params.userId;
+    console.log(`User ID from URL: ${userId}`);
+  })
 
   const app = new Application();
+
+  app.use(logger);
+
   app.use(router.routes());
   app.use(router.allowedMethods());
 
